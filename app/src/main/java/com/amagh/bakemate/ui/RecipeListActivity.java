468552f40ce05ce754f975eca1afbeeb07f60e98
Alvin Magh <amagh1990@gmail.com>
2017-06-27 15:38:17 -0700

    Refactor RecipeListActivity
    Moved file to new ui package for better organization.

--- app/src/main/java/com/amagh/bakemate/ui/RecipeListActivity.java
+++ app/src/main/java/com/amagh/bakemate/ui/RecipeListActivity.java
@@ -1,7 +1,9 @@
-package com.amagh.bakemate;
+package com.amagh.bakemate.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.amagh.bakemate.R;
 
 public class RecipeListActivity extends AppCompatActivity {
 
