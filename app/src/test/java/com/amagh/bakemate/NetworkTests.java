e47e80221f4a4f5c496961196128fb22a34c18b8
Alvin Magh <amagh1990@gmail.com>
2017-06-27 14:11:24 -0700

    Changed URL for recipes
    Original link re-directs to the JSON file. This causes a problem when
    attempting to retrieve the HTTP response as it returns null.

--- app/src/test/java/com/amagh/bakemate/NetworkTests.java
+++ app/src/test/java/com/amagh/bakemate/NetworkTests.java
@@ -31,14 +31,14 @@ public class NetworkTests {
 
     @Test
     public void testNetworkResponse() {
-        String urlString = "http://go.udacity.com/android-baking-app-json";
+        String urlString = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 
         try {
             String response = NetworkUtils.getHttpResponse(urlString);
 
             String errorNoNetworkResponse = "No response returned when connecting to " + urlString +
                     ". Have sufficient permissions been added to the Manifest?";
-            assertNotNull(response, errorNoNetworkResponse);
+            assertNotNull(errorNoNetworkResponse, response);
 
         } catch (IOException e) {
             e.printStackTrace();
