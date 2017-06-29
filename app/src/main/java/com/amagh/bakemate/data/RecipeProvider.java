d4f2f97d68aaa746d54d99e8df50afe7367f9ca3
Alvin Magh <amagh1990@gmail.com>
2017-06-28 19:04:13 -0700

    Add InexactContentUris for the ingredients and steps tables
    Uris are for accessing the ingredients and steps for a specific recipe.

--- app/src/main/java/com/amagh/bakemate/data/RecipeProvider.java
+++ app/src/main/java/com/amagh/bakemate/data/RecipeProvider.java
@@ -19,6 +19,13 @@ import net.simonvt.schematic.annotation.TableEndpoint;
 public class RecipeProvider {
 
     public static final String AUTHORITY = "com.amagh.bakemate.provider";
+    public static final String JOIN_RECIPE_INGREDIENT_STEP =
+            "JOIN " + RecipeDatabase.INGREDIENTS + " ON "                                       +
+            RecipeDatabase.RECIPES + "." + RecipeContract.RecipeEntry.COLUMN_RECIPE_ID + " = "  +
+            RecipeDatabase.INGREDIENTS + "." + RecipeContract.IngredientEntry.COLUMN_RECIPE_ID  +
+            "JOIN" + RecipeDatabase.STEPS + " ON "                                              +
+            RecipeDatabase.RECIPES + "." + RecipeContract.RecipeEntry.COLUMN_RECIPE_ID + " = "  +
+            RecipeDatabase.STEPS + "." + RecipeContract.StepEntry.COLUMN_RECIPE_ID;
 
     interface Path {
         String RECIPES = "recipes";
@@ -30,15 +37,15 @@ public class RecipeProvider {
     public static class Recipes {
 
         @ContentUri(
-                path = Path.RECIPES,
-                type = "vnd.android.cursor.dir/recipe",
+                path        = Path.RECIPES,
+                type        = "vnd.android.cursor.dir/recipe",
                 defaultSort = RecipeContract.RecipeEntry.COLUMN_RECIPE_ID + " ASC")
         public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/recipes");
 
         @InexactContentUri(
-                path = Path.RECIPES + "/#",
-                name = "RECIPE_ID",
-                type = "vnd.android.cursor.item/recipe",
+                path        = Path.RECIPES + "/#",
+                name        = "RECIPE_ID",
+                type        = "vnd.android.cursor.item/recipe",
                 whereColumn = RecipeContract.RecipeEntry.COLUMN_RECIPE_ID,
                 pathSegment = 1)
         public static Uri withId(long recipeId) {
@@ -50,10 +57,24 @@ public class RecipeProvider {
     public static class Ingredients {
 
         @ContentUri(
-                path = "ingredients",
-                type = "vnd.android.cursor.dir/ingredient",
+                path        = Path.INGREDIENTS,
+                type        = "vnd.android.cursor.dir/ingredient",
                 defaultSort = RecipeContract.IngredientEntry.COLUMN_ID + " ASC")
         public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/ingredients");
+
+        @InexactContentUri(
+                path        = Path.INGREDIENTS + "/" + Path.RECIPES + "/#",
+                name        = "INGREDIENTS_WITH_RECIPE_ID",
+                type        = "vnd.android.cursor.dir/ingredient",
+                whereColumn = RecipeContract.IngredientEntry.COLUMN_RECIPE_ID,
+                pathSegment = 2,
+                defaultSort = RecipeContract.IngredientEntry.COLUMN_MEASURE + " ASC")
+        public static Uri forRecipe(long recipeId) {
+            return Recipes.CONTENT_URI.buildUpon()
+                    .appendPath(Path.RECIPES)
+                    .appendPath(Long.toString(recipeId))
+                    .build();
+        }
 
     }
 
@@ -61,9 +82,23 @@ public class RecipeProvider {
     public static class Steps {
 
         @ContentUri(
-                path = "steps",
-                type = "vnd.android.cursor.dir/step",
+                path        = Path.STEPS,
+                type        = "vnd.android.cursor.dir/step",
                 defaultSort = RecipeContract.StepEntry.COLUMN_STEP_ID + " ASC")
         public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/steps");
+
+        @InexactContentUri(
+                path        = Path.STEPS + "/" + Path.RECIPES + "/#",
+                name        = "STEPS_WITH_RECIPE_ID",
+                type        = "vnd.android.cursor.dir/step",
+                whereColumn = RecipeContract.StepEntry.COLUMN_RECIPE_ID,
+                pathSegment = 2,
+                defaultSort = RecipeContract.StepEntry.COLUMN_STEP_ID + " ASC")
+        public static Uri forRecipe(long recipeId) {
+            return CONTENT_URI.buildUpon()
+                    .appendPath(Path.RECIPES)
+                    .appendPath(Long.toString(recipeId))
+                    .build();
+        }
     }
 }
