af4885e8d82ff2c9a2657a525e1bbac1c3d10961
Alvin Magh <amagh1990@gmail.com>
2017-06-29 11:03:13 -0700

    Add layout for recipe details list item

---
+++ app/src/main/java/com/amagh/bakemate/models/Recipe.java
@@ -0,0 +1,35 @@
+package com.amagh.bakemate.models;
+
+import android.content.Context;
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.amagh.bakemate.R;
+
+/**
+ * Created by hnoct on 6/29/2017.
+ */
+
+public class Recipe extends BaseObservable {
+    private long recipeId;
+    private String name;
+    private int servings;
+    private Context mContext;
+
+    public Recipe(Context context, long recipeId, String name, int servings) {
+        this.recipeId = recipeId;
+        this.name = name;
+        this.servings = servings;
+        mContext = context;
+    }
+
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    @Bindable
+    public String getServings() {
+        return mContext.getString(R.string.list_details_servings_format, servings);
+    }
+}
