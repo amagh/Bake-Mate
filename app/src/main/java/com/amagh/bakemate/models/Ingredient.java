125314fab9580012a4ecf8728bc9bdcdf5058536
Alvin Magh <amagh1990@gmail.com>
2017-06-29 17:39:27 -0700

    Update layout for item list item to combine quantity and measure
    When separated, it makes for awkward formatting for some ingredients
    that do not include a measure (e.g. 5 eggs). Also added method to
    convert the database-stored measure to a more readable one (e.g.
    "TBLSP" => "Tbsp")

--- app/src/main/java/com/amagh/bakemate/models/Ingredient.java
+++ app/src/main/java/com/amagh/bakemate/models/Ingredient.java
@@ -1,18 +1,26 @@
 package com.amagh.bakemate.models;
 
+import android.content.Context;
 import android.databinding.BaseObservable;
 import android.databinding.Bindable;
+
+import com.amagh.bakemate.R;
+
+import java.util.Locale;
 
 /**
  * ViewModel for the Ingredient List Item
  */
 
 public class Ingredient extends BaseObservable {
-    private int quantity;
+    // **Member Variables** //
+    private Context mContext;
+    private double quantity;
     private String measure;
     private String ingredient;
 
-    public Ingredient(int quantity, String measure, String ingredient) {
+    public Ingredient(Context context, double quantity, String measure, String ingredient) {
+        mContext = context;
         this.quantity = quantity;
         this.measure = measure;
         this.ingredient = ingredient;
@@ -20,16 +28,56 @@ public class Ingredient extends BaseObservable {
 
     @Bindable
     public String getQuantity() {
-        return Integer.toString(quantity);
-    }
+        String measure = null;
 
-    @Bindable
-    public String getMeasure() {
-        return measure;
+        // Set the measure to more readable String
+        switch (this.measure) {
+            case "G":
+                measure = mContext.getString(R.string.measurement_gram);
+                break;
+            case "TBLSP":
+                measure = mContext.getString(R.string.measurement_tablespoon);
+                break;
+            case "TSP":
+                measure = mContext.getString(R.string.measurement_teaspoon);
+                break;
+            case "UNIT":
+                measure = "";
+                break;
+            case "K":
+                measure = mContext.getString(R.string.measurement_package);
+                break;
+            case "CUP":
+                measure = mContext.getString(R.string.measurement_cup);
+                break;
+            case "OZ":
+                measure = mContext.getString(R.string.measurement_ounce);
+                break;
+        }
+
+        // Check if quantity contains a decimal or if it can be boxed to an equivalent int
+        if (quantity == (int) quantity) {
+            return String.format(Locale.getDefault(), "%d %s", (int) quantity, measure);
+        } else {
+            return String.format(Locale.getDefault(), "%s %s", convertDecimalToFraction(quantity), measure);
+        }
     }
 
     @Bindable
     public String getIngredient() {
         return ingredient;
+    }
+
+    /**
+     * Helper method for converting decimals to fractions for easier readability
+     * @param quantity
+     * @return
+     */
+    private String convertDecimalToFraction(double quantity) {
+        if ((int) quantity > 0) {
+            return (int) quantity + "\u00bd";
+        } else {
+            return "\u00bd";
+        }
     }
 }
